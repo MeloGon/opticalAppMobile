4a00a18c6b0623a6e324c9989b1bcdcb8eff7104
Kevyn Melo <troy.esname@gmail.com>
2023-09-04 19:19:58 -0500

    add client model for snapshot of firestore response

--- lib/models/client.dart
+++ lib/models/client.dart
@@ -71,7 +71,7 @@ class Client {
       oiEjeCe: json['oi_eje_ce'],
       oiAdd: json['oi_add'],
       deDp: json['de_dp'],
-      izDp: json['de_dp'],
+      izDp: json['iz_dp'],
       totalDp: json['dp_total'],
     );
   }
@@ -79,4 +79,55 @@ class Client {
   // factory Client.fromSnapshot(DocumentSnapshot) {
   //  Client client =
   // }
+
+  // factory Client.fromFirestore(Map<dynamic, dynamic> json) {
+  //   return Client(
+  //     userId: json['userId'],
+  //     dateCreated: json['fecha_cli'],
+  //     name: json['nombre_cli'],
+  //     numberPhone: json['cel_cli'],
+  //     vendeCli: json['vende_cli'],
+  //     odEsfLe: json['od_esf_le'],
+  //     odCilLe: json['od_cil_le'],
+  //     odEjeLe: json['od_eje_le'],
+  //     odEsfCe: json['od_esf_ce'],
+  //     odCilCe: json['od_cil_ce'],
+  //     odEjeCe: json['od_eje_ce'],
+  //     odAdd: json['od_add'],
+  //     oiEsfLe: json['oi_esf_le'],
+  //     oiCilLe: json['oi_cil_le'],
+  //     oiEjeLe: json['oi_eje_le'],
+  //     oiEsfCe: json['oi_esf_ce'],
+  //     oiCilCe: json['oi_cil_ce'],
+  //     oiEjeCe: json['oi_eje_ce'],
+  //     oiAdd: json['oi_add'],
+  //     deDp: json['de_dp'],
+  //     izDp: json['de_dp'],
+  //     totalDp: json['dp_total'],
+  //   );
+  // }
+
+  Client.fromDocumentSnapshot(DocumentSnapshot<Map<String, dynamic>> doc)
+      : userId = doc.id,
+        dateCreated = doc.data()!['fecha_cli'],
+        name = doc.data()!['nombre_cli'],
+        numberPhone = doc.data()!['cel_cli'],
+        vendeCli = doc.data()!['vende_cli'],
+        odEsfLe = doc.data()!['od_esf_le'],
+        odCilLe = doc.data()!['od_cil_le'],
+        odEjeLe = doc.data()!['od_eje_le'],
+        odEsfCe = doc.data()!['od_esf_ce'],
+        odCilCe = doc.data()!['od_cil_ce'],
+        odEjeCe = doc.data()!['od_eje_ce'],
+        odAdd = doc.data()!['od_add'],
+        oiEsfLe = doc.data()!['oi_esf_le'],
+        oiCilLe = doc.data()!['oi_cil_le'],
+        oiEjeLe = doc.data()!['oi_eje_le'],
+        oiEsfCe = doc.data()!['oi_esf_ce'],
+        oiCilCe = doc.data()!['oi_cil_ce'],
+        oiEjeCe = doc.data()!['oi_eje_ce'],
+        oiAdd = doc.data()!['oi_add'],
+        deDp = doc.data()!['de_dp'],
+        izDp = doc.data()!['iz_dp'],
+        totalDp = doc.data()!['dp_total'];
 }
